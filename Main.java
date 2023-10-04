b1b7e380cf9d6015baa2a7ee4ac8005478ec8131
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-04 15:11:44 +0200

    Completed first exercise

---
+++ Main.java
@@ -0,0 +1,39 @@
+import entities.Rettangolo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rettangolo primoRettangolo = new Rettangolo(12.4, 7.5);
+        Rettangolo secondoRettangolo = new Rettangolo(3.9, 18);
+
+        System.out.println(primoRettangolo.area());
+        System.out.println(secondoRettangolo.perimetro());
+
+        stampaRettangolo(primoRettangolo);
+        stampaDueRettangoli(primoRettangolo, secondoRettangolo);
+    }
+
+    public static void stampaRettangolo(Object o) {
+        if (o.getClass() != entities.Rettangolo.class) {
+            System.out.println("Questo metodo funziona solo sui rettangoli.");
+        } else {
+            Rettangolo rettangolo = (Rettangolo) o;
+            System.out.println("Perimetro = " + rettangolo.perimetro() + " Area = " + rettangolo.area());
+        }
+    }
+
+    public static void stampaDueRettangoli(Object o1, Object o2) {
+        if (o1.getClass() != entities.Rettangolo.class || o2.getClass() != entities.Rettangolo.class) {
+            System.out.println("Questo metodo funziona solo sui rettangoli.");
+
+        } else {
+            Rettangolo rettangolo1 = (Rettangolo) o1;
+            Rettangolo rettangolo2 = (Rettangolo) o2;
+            double sommaPerimetri = rettangolo1.perimetro() + rettangolo2.perimetro();
+            double sommaArea = rettangolo1.area() + rettangolo2.area();
+            System.out.println("Perimetro 1 = " + rettangolo1.perimetro() + " Area 1 = " + rettangolo1.area());
+            System.out.println("Perimetro 2 = " + rettangolo2.perimetro() + " Area 2 = " + rettangolo2.area());
+            System.out.println("La somma dei perimetri è = " + sommaPerimetri + " La somma delle aree è = " + sommaArea);
+        }
+    }
+}
---
+++ entities/Rettangolo.java
@@ -0,0 +1,46 @@
+package entities;
+
+
+public class Rettangolo {
+
+    private double height;
+    private double width;
+
+    public Rettangolo(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double perimetro() {
+        return (this.height + this.width) * 2;
+    }
+
+    public double area() {
+        return this.height * this.width;
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height > 0) {
+            this.height = height;
+        } else {
+            System.out.println("Inserisci un'altezza maggiore di 0");
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width > 0) {
+            this.width = width;
+        } else {
+            System.out.println("Inserisci una larghezza maggiore di 0");
+        }
+    }
+}
